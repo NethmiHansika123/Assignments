62a4d6f515c54f7ea71070b627e124c6533cc7fd
NethmiHansika123 <101331394+NethmiHansika123@users.noreply.github.com>
2022-04-18 11:08:00 +0530

    Update App.js

--- bootstrapgrid/bootstrapgrid/src/App.js
+++ bootstrapgrid/bootstrapgrid/src/App.js
@@ -1,17 +1,74 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Container from './components/container';
-import Assignment from './components/assignment';
-import FlexContainer from './components/flexboxes/flexcontainer';
-import FlexOrder from './components/flexboxes/flexorder';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Container from "./components/container";
+import Assignment from "./components/assignment";
+import FlexContainer from "./components/flexboxes/flexcontainer";
+import FlexOrder from "./components/flexboxes/flexorder";
+import Todolist from './components/todolist/todolist';
+import React, { useState } from "react";
+import Form from "./Form";
 
 
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = i =>
+  setTodos(
+    todos.map(
+      (todo, k) =>
+        k === i
+          ? {
+              ...todo,
+              complete: !todo.complete
+            }
+          : todo
+    )
+  );
+    const deleteItem = (i) => {
+      const newList = todos.filter((todo) => todo.key !== i);
+           todos.map((todo, k) =>
+                 k === i,
+                 todos.filter((todo) => todo.k !== i),
+                 setTodos(todos)
+       )
+      
+     }
+
+  
   return (
     <div className="App">
-     <Router>
-     <Switch>
-          <Route  exact path="/">
+      <div className="">
+      {/* <Form
+        onSubmit={(text) => setTodos([{ text, complete: false,key:Date.now() }, ...todos])}
+      /> */}
+    
+        
+          {todos.map(({ text, complete }, i) => (
+            <div
+              // key={text}
+              onClick={() => toggleComplete(i)}
+              style={{
+                textDecoration: complete ? "line-through" : "",
+
+              }}
+              
+             
+            >
+              <button onClick={() => delete(text.key)}>del</button>
+              
+              {text}
+             
+            </div>
+          ))}
+           
+      
+        {/* <button onClick={() => setTodos([])}><b>Reset</b></button> */}
+        
+        </div>
+        
+        <Router>
+          <Switch>
+          <Route  exact path="/container">
             <Container />
           </Route>
           <Route exact path="/assignment">
@@ -23,10 +80,13 @@ function App() {
           <Route exact path="/flexorder">
             <FlexOrder />
           </Route>
+          <Route exact path="/todolist">
+            <Todolist />
+          </Route>
          
         </Switch>
      </Router>
-   </div>
+    </div>
   );
 }
 
